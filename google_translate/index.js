238427962cddcea38a8325701750f4cd2fd61663
akshit <akin.park@gmail.com>
2023-04-11 15:05:07 +0530

    feat(no-breaking-change): - Adds integration with google translate

---
+++ google_translate/index.js
@@ -0,0 +1,43 @@
+const { Translate } = require("@google-cloud/translate").v2;
+const dotenv = require("dotenv");
+dotenv.config();
+
+function privateKey(val) {
+  if (!val) {
+    return null;
+  }
+  const content = val.split(":").join("\n");
+  return `-----BEGIN PRIVATE KEY-----\n${content}\n-----END PRIVATE KEY-----\n`;
+}
+
+const googleCreds = {
+  projectId: process.env.GOOGLE_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CREDENTIALS_CLIENT_EMAIL,
+    private_key: privateKey(process.env.GOOGLE_CREDENTIALS_PRIVATE_KEY),
+  },
+};
+
+const translate = new Translate(googleCreds);
+
+const text = "I spea russian";
+
+/**
+ * 
+ * @param {string} text The text that needs to be translated
+ * @param {string} from The source language
+ * @param {string} to The destination language
+ * @returns {string} Translated text
+ */
+async function translateText(text, from, to) {
+  const translationOptions = { from, to };
+  let [translation] = await translate.translate(text, translationOptions);
+  return translation;
+}
+
+async function main() {
+  const translatedText = await translateText(text, 'en', 'ru');
+  console.log('response: ', translatedText);
+}
+
+main();
