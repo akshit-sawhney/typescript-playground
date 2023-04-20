82ceac888e7ab096c87b4a9d99325c43e0e07492
akshit <akin.park@gmail.com>
2023-04-20 14:17:53 +0530

    feat(siblings-query): - Adds queries to get the siblings of a node

--- worksheets_v2/without_grade_workflow/mongo/mongo_queries.js
+++ worksheets_v2/without_grade_workflow/mongo/mongo_queries.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let WorksheetHierarchies;
 
@@ -12,49 +12,74 @@ const worksheetHierarchySchema = new mongoose.Schema({
 });
 
 async function boot() {
-  await mongoose.connect('mongodb://localhost:27017', {
+  await mongoose.connect("mongodb://localhost:27017", {
     // autoReconnect: true,
-    dbName: 'quizizz',
+    dbName: "quizizz",
   });
   WorksheetHierarchies = mongoose.model(
-    'worksheetHierarchy',
+    "worksheetHierarchy",
     worksheetHierarchySchema,
-    'worksheetHierarchy',
+    "worksheetHierarchy"
   );
 }
 
 async function getAllGrades() {
-    const docs = await WorksheetHierarchies.find({path: '/'});
-    console.log('response: ', docs.length);
+  const docs = await WorksheetHierarchies.find({ path: "/" });
+  console.log("response: ", docs.length);
 }
 
 async function searchByKeyword(keyword, gradeName) {
-    const searchPrompt = {
-        name: keyword
-    }
-    if (gradeName) {
-        searchPrompt.grade = gradeName;
-    } else {
-        searchPrompt.grade = 'no-grade';
-    }
-    const docs = await WorksheetHierarchies.find(searchPrompt);
-    console.log('searchByKeyword response: ', docs);
+  const searchPrompt = {
+    name: keyword,
+  };
+  if (gradeName) {
+    searchPrompt.grade = gradeName;
+  } else {
+    searchPrompt.grade = "no-grade";
+  }
+  const docs = await WorksheetHierarchies.find(searchPrompt);
+  console.log("searchByKeyword response: ", docs);
 }
 
 async function getImmediateChildren(parentNodeId) {
-    const searchPrompt = {
-        path: new RegExp(`\/\\w+\/${parentNodeId}\/`)
-    }
-    console.log('search promt: ', searchPrompt)
-    const docs = await WorksheetHierarchies.find(searchPrompt);
-    console.log('getImmediateChildren response: ', docs);
+  const searchPrompt = {
+    path: new RegExp(`\/\\w+\/${parentNodeId}\/`),
+  };
+  console.log("search promt: ", searchPrompt);
+  const docs = await WorksheetHierarchies.find(searchPrompt);
+  console.log("getImmediateChildren response: ", docs);
+}
+
+async function getSiblings(currentNodeName, gradeName) {
+  const searchPrompt = {
+    name: currentNodeName,
+  };
+  if (gradeName) {
+    searchPrompt.grade = gradeName;
+  } else {
+    searchPrompt.grade = "no-grade";
+  }
+  const doc = await WorksheetHierarchies.findOne(searchPrompt);
+  const siblingSearchPrompt = {
+    path: doc.path,
+  };
+  if (gradeName) {
+    searchPrompt.grade = gradeName;
+  } else {
+    searchPrompt.grade = "no-grade";
+  }
+  const siblingDocs = await WorksheetHierarchies.find(siblingSearchPrompt);
+  console.log("getSiblings response: ", siblingDocs);
+  return siblingDocs;
 }
 
 async function main() {
   await boot();
-//   await getAllGrades();
-//   await searchByKeyword('the-arts', 'kg');
-  await getImmediateChildren('643f81d6ebeef01c2b8d4f4a');
+  //   await getAllGrades();
+  //   await searchByKeyword('the-arts', 'kg');
+  // await getImmediateChildren("643f81d6ebeef01c2b8d4f4a");
+  await getSiblings("colors", "kg");
+  process.exit(0);
 }
 
 main();
